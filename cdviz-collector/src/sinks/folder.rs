46fe48f9b002e8bcdca46d338c48661f96edca4a
David Bernard <david.bernard.31@gmail.com>
2024-10-06 17:06:41 +0200

    chore: add a default configuration for sink `cdevents_local_json`

--- cdviz-collector/src/sinks/folder.rs
+++ cdviz-collector/src/sinks/folder.rs
@@ -46,11 +46,22 @@ mod tests {
     use assert2::let_assert;
     use cdevents_sdk::CDEvent;
     use test_strategy::proptest;
+    use std::path::PathBuf;
+
+    fn assert_eq(cdevent: &CDEvent, file: PathBuf) {
+        assert!(file.exists());
+        let example_txt = std::fs::read_to_string(file).unwrap();
+        let example_json: serde_json::Value =
+            serde_json::from_str(&example_txt).expect("to parse as json");
+        let example: CDEvent =
+            serde_json::from_value(example_json.clone()).expect("to parse as cdevent");
+        assert_eq!(&example, cdevent);
+    }
 
     #[proptest(async = "tokio", cases = 10)]
-    //TODO reuse same sink for all tests (but be sure to drop it after)
-    async fn test_send_valid(#[any] cdevent0: CDEvent) {
-        // TODO allow any id (not only uuid) or change the policy in cdevents
+    //TODO reuse same sink for all tests (but be sure to drop it after, look at db test)
+    async fn test_send_random_cdevents(#[any] cdevent0: CDEvent) {
+        //TODO allow any id (not only uuid) or change the policy in cdevents
         let cdevent = cdevent0.with_id(uuid::Uuid::new_v4().to_string().try_into().unwrap());
         let tmp_dir = tempfile::tempdir().unwrap();
         let config = Config {
@@ -68,13 +79,7 @@ mod tests {
         let file = tmp_dir.path().join(format!("{id}.json"));
         assert!(!file.exists());
         let_assert!(Ok(()) = sink.send(&msg).await);
-        assert!(file.exists());
-        let example_txt = std::fs::read_to_string(file).unwrap();
-        let example_json: serde_json::Value =
-            serde_json::from_str(&example_txt).expect("to parse as json");
-        let example: CDEvent =
-            serde_json::from_value(example_json.clone()).expect("to parse as cdevent");
-        assert_eq!(example, cdevent);
+        assert_eq(&cdevent, file);
         proptest::prop_assert!(true);
     }
 }
