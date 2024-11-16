0e0f13e8655812802cdbd0759e51f86ebca801d7
David Bernard <david.bernard.31@gmail.com>
2024-11-16 19:31:24 +0100

    wip

--- cdviz-site/landingpage/main.js
+++ cdviz-site/landingpage/main.js
@@ -1,1 +1,6 @@
+import "./style.css";
 
+const heroImage = document.getElementById("hero-image-img");
+const randomInt = Math.floor(Math.random() * 8);
+heroImage.src = `./assets/illustrations/hero-${randomInt}.webp`;
+console.log(randomInt);
--- cdviz-site/landingpage/tailwind.config.js
+++ cdviz-site/landingpage/tailwind.config.js
@@ -1,8 +1,44 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      // fontSize: {
+      //   sm: "0.750rem",
+      //   base: "1rem",
+      //   xl: "1.333rem",
+      //   "2xl": "1.777rem",
+      //   "3xl": "2.369rem",
+      //   "4xl": "3.158rem",
+      //   "5xl": "4.210rem",
+      // },
+      fontFamily: {
+        sans: ["Droid Sans", ...defaultTheme.fontFamily.sans],
+        heading: "Droid Sans",
+        body: "Droid Sans",
+      },
+      fontWeight: {
+        normal: "400",
+        bold: "700",
+      },
+      colors: {
+        // text: "#031026",
+        // background: "#e7ecf3",
+        // primary: "#f39208", //"#ff6600",
+        // secondary: "#685ff2",
+        // accent: "#3d12d9",
+        'text': 'var(--text)',
+        'background': 'var(--background)',
+        'primary': 'var(--primary)',
+        'secondary': 'var(--secondary)',
+        'accent': 'var(--accent)',
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+  darkMode: "selector",
 };
