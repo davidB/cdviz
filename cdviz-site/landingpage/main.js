f402495a967d283fdcdd42efa694d0f4c0166c97
David Bernard <david.bernard.31@gmail.com>
2024-11-17 20:36:11 +0100

    ci: no megalinter on html/css (not tailwindcss compatible)

--- cdviz-site/landingpage/main.js
+++ cdviz-site/landingpage/main.js
@@ -1,6 +1,9 @@
 import "./style.css";
 
-const heroImage = document.getElementById("hero-image-img");
-const randomInt = Math.floor(Math.random() * 8);
-heroImage.src = `./assets/illustrations/hero-${randomInt}.webp`;
-console.log(randomInt);
+function selectHeroImage() {
+  const randomInt = Math.floor(Math.random() * 8);
+  const heroImage = document.getElementById("hero-image-img");
+  heroImage.src = `./assets/illustrations/hero-${randomInt}.webp`;
+}
+
+selectHeroImage();
