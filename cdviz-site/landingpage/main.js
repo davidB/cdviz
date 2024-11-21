1addb827a145e6d991578adcac36ac2f57a59a44
David Bernard <david.bernard.31@gmail.com>
2024-11-21 22:13:43 +0100

    style(cdviz-site): basic autodetection of drask mode

--- cdviz-site/landingpage/main.js
+++ cdviz-site/landingpage/main.js
@@ -6,4 +6,17 @@ function selectHeroImage() {
   heroImage.src = `./assets/illustrations/hero-${randomInt}.webp`;
 }
 
+function initColorMode() {
+  // It's best to inline this in `head` to avoid FOUC (flash of unstyled content) when changing pages or themes
+  if (
+    localStorage.getItem("color-theme") === "dark" ||
+    (!("color-theme" in localStorage) &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches)
+  ) {
+    document.documentElement.classList.add("dark");
+  } else {
+    document.documentElement.classList.remove("dark");
+  }
+}
+initColorMode();
 selectHeroImage();
